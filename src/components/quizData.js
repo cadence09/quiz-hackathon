3d87e167750e92a98415af1b0523607d6823b81e
Caden <cadence1547@gmail.com>
2020-07-15 10:50:10 -0700

    add questions

--- src/components/quizData.js
+++ src/components/quizData.js
@@ -1,26 +1,39 @@
-export const quizData = [
-  {
-    id: 0,
-    question: ` What is the Capital Of India ?`,
-    options: [`New Delhi`, `Mumbai`, `Kolkatta`],
-    answer: `New Delhi`,
-  },
-  {
-    id: 1,
-    question: `Who is the CEO of Tesla Motors?`,
-    options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
-    answer: `Elon Musk`,
-  },
-  {
-    id: 3,
-    question: `Name World's Richest Man?`,
-    options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
-    answer: `Jeff Bezo`,
-  },
-  {
-    id: 4,
-    question: `World's Longest River?`,
-    options: [`River Nile`, `River Amazon`, `River Godavari`],
-    answer: `River Nile`,
-  },
-];
+export const quizData=[
+    {
+        "id":0,
+        "question": "Which of the following is correct about callbacks?",
+        "options": ["A callback is a function that passes to another function as an argument.", "A callback is not a function that passes to another function as an argument", "None of above"],
+        "answer": ["A callback is a function that passes to another function as an argument."]
+    },
+    {   "id":1,
+        "question":"Which of the following function of Number object returns a string value version of the current number?",
+        "options":["toString()", "toFixed()","toLocaleString()","toPrecision()"],
+        "answer":"toString()"
+    },
+    {   "id":2,
+        "question":"what thread is JavaScript",
+        "options":["single thread", "multithread", "none of above"],
+        "answer":"single thread"
+},
+    {   
+        "id":3,
+        "question":"If we declare a variable, let test = 1, then later, reassign, stating test = 2, what will happen?",
+        "options":["test will equal 2", "test will equal 1","JavaScript will raise a TypeError", "test will equal undefined"],
+        "answer":"test will equal 2"
+    },
+    {   "id":4,
+        "question":"What does undefined value mean in javascript?",
+        "options":["undefine means a variable has been declared but has not yet been assigned a value", "Undefine means function didn’t return any value", "All above", "None of above"],
+        "answer":"All of above"
+    },
+    {   "id":5,
+        "question":"In your JavaScript code, how do you find out which character occurs at the 5th position in a string, How are you?",
+        "options":["Substring()"," String()","Stringlength()"," CharAt()"],
+        "answer":"ChatAt()"
+    },
+    {   "id":6,
+        "question":"How do you prompt users with messages and at the same time requesting user inputs?",
+        "options":["Alert()","Display()", " Prompt()", "Confirm()"],
+        "answer":"Prompt()",
+    }
+]
