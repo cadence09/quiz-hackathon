29052f6aac49a55f8f01457e984aa9b48c1de799
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-07-16 22:16:07 -0600

    update the names

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-import React from "react";
+// import React from "react";
 import QuizApp from "./components/QuizApp";
+import { questionsAndAnswers } from "./components/questionsAndAnswers";
 let inspect = require("inspect.js");
 
 describe("MyClass", function () {
@@ -8,22 +8,15 @@ describe("MyClass", function () {
     inspect(QuizApp).isClass();
   });
 });
-=======
-import { quizData } from './components/quizData';
-let inspect = require('inspect.js');
 
-
-describe('quizData', function() {
-  it('checks if file is a JSON object', () =>{
-      inspect(quizData).isJSON();
-  })
+describe("questionsAndAnswers", function () {
+  it("checks if file is a JSON object", () => {
+    inspect(questionsAndAnswers).isJSON();
+  });
 });
 
-
-describe('MyClass', function() {
-  it('first unit test', ()=>{
-
-    inspect(MainQuiz).isClass();
-})
-  })
->>>>>>> 9b423b1f448a7246486b50ec079e12d72d213f91
+describe("MyClass", function () {
+  it("first unit test", () => {
+    inspect(QuizApp).isClass();
+  });
+});
