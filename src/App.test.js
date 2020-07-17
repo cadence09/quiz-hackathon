243034f51f2645e1f26b566a2b9e8d6872b41d33
TaLea Carpenter <taleacarpenter@gmail.com>
2020-07-17 01:55:36 -0700

    update test file.

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import { quizData } from './components/quizData';
-let inspect = require('inspect.js');
-=======
-// import React from "react";
 import QuizApp from "./components/QuizApp";
 import { questionsAndAnswers } from "./components/questionsAndAnswers";
 let inspect = require("inspect.js");
 
-describe("MyClass", function () {
-  it("first unit test", () => {
-    inspect(QuizApp).isClass();
-  });
-});
->>>>>>> 12e0129ccb90138f748c8bbcf8ee5e5027db205d
 
 describe("questionsAndAnswers", function () {
   it("checks if file is a JSON object", () => {
@@ -21,12 +10,9 @@ describe("questionsAndAnswers", function () {
 });
 
 
-<<<<<<< HEAD
 describe('MyClass', function() {
   it('first unit test', ()=>{
 
     inspect(MainQuiz).isClass();
 })
   })
-=======
->>>>>>> 12e0129ccb90138f748c8bbcf8ee5e5027db205d
