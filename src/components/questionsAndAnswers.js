05dc0e50e6b1bb442d7e6082803a7396e2f6a716
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-07-17 12:48:41 -0600

    add minor changes to questions

--- src/components/questionsAndAnswers.js
+++ src/components/questionsAndAnswers.js
@@ -46,18 +46,11 @@ export const questionsAndAnswers = [
 
   {
     id: 4,
-    question: "setTimeOut() is executed by a Web API",
-    options: ["True", "False"],
-    answer: "True",
-  },
-
-  {
-    id: 5,
     question: "How do you debug code in JavaScript?",
     options: [
       "Use breakpoints on Chrome DevTool",
-      "Console.log()",
-      "Debugger",
+      "console.log()",
+      "debugger",
       "All of the above",
       "None of the above",
     ],
@@ -65,22 +58,22 @@ export const questionsAndAnswers = [
   },
 
   {
-    id: 6,
+    id: 5,
     question:
       "How do you prompt users with messages and at the same time request user input?",
-    options: ["Alert()", "Display()", " Prompt()", "Confirm()"],
-    answer: "Prompt()",
+    options: ["alert()", "display()", " prompt()", "confirm()"],
+    answer: "prompt()",
   },
 
   {
-    id: 7,
+    id: 6,
     question: "Javascript is not asynchronous",
     options: ["True", "False"],
     answer: "False",
   },
 
   {
-    id: 8,
+    id: 7,
     question: "What is the difference between an argument and a parameter?",
     options: [
       "An argument is a variable declared in a function, a parameter is an actual value that is passed to the function",
@@ -93,36 +86,36 @@ export const questionsAndAnswers = [
   },
 
   {
-    id: 9,
+    id: 8,
     question: "setTimeOut() is executed by Web API",
     options: ["True", "False"],
     answer: "True",
   },
 
   {
-    id: 10,
+    id: 9,
     question: "What is new in ES6?",
     options: [
       "Symbol data type",
       "The let and const keywords",
       "Arrow function",
       "B and C, but not A",
-      "All of the above",
-      "None of the above",
+      "A, B, C",
+      "A, B",
     ],
-    answer: "All of the above",
+    answer: "A, B, C",
+  },
+
+  {
+    id: 10,
+    question:
+      "How do you find out which character occurs at the 5th position in a string `How are you`?",
+    options: ["substring()", " string()", "stringlength()", " charAt()"],
+    answer: "charAt()",
   },
 
   {
     id: 11,
-    question:
-      "How do you find out which character occurs at the 5th position in a string `How are you`?",
-    options: ["Substring()", " String()", "Stringlength()", " CharAt()"],
-    answer: "CharAt()",
-  },
-
-  {
-    id: 12,
     question: "What is lexical scope?",
     options: [
       "A variable that can be accessed by all other functions",
